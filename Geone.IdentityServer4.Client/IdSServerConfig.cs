c38cb05b1c4bf04ce97cf4e5cc274d0fdf4cd8db
hongsh <hongshouhao@hotmail.com>
2020-04-30 13:40:28 +0800

    删除 Geone.IdentityServer4.Client\IdSServerConfig.cs

--- Geone.IdentityServer4.Client/IdSServerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Geone.IdentityServer4.Client
-{
-    public class IdSServerConfig
-    {
-        public string BaseUrl { get; set; }
-        public string ApiSecret { get; set; }
-        public string ApiName { get; set; }
-    }
-}
